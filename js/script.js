baa7cb1c55c5e2b1cbd0db409291d83221a01353
Alfred <alfred.adom@gmail.com>
2019-06-21 01:59:18 -0400

    hiding options via jquery on load

---
+++ js/script.js
@@ -0,0 +1,12 @@
+//project 3
+$("#name").focus();
+
+$("#other-title").hide();
+
+$("#design option")
+  .eq(0)
+  .hide();
+
+$("#colors-js-puns label").html("Please select a T-shirt theme");
+
+$("#color").hide();
