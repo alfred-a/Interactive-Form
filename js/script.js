ef44f13332d6038f8f976d3b6aac34d61d65c8dc
Alfred <alfred.adom@gmail.com>
2019-06-22 09:46:02 -0400

    added classes to t shirts in html

--- js/script.js
+++ js/script.js
@@ -10,3 +10,17 @@ $("#design option")
 $("#colors-js-puns label").html("Please select a T-shirt theme");
 
 $("#color").hide();
+
+$("#design").change(function() {
+  let selectedValue = $(this).val();
+  if (selectedValue == "js puns") {
+    $(".JS Puns shirt").show();
+  }
+});
+
+$("#design").change(function() {
+  let selectedValue = $(this).val();
+  if (selectedValue == "heart js") {
+    $(".JS shirt").show();
+  }
+});
